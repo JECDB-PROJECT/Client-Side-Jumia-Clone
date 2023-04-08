56cc48af519222bba13aa07643d8798a872daf1f
Ahmed <engahmedabdultawab@gmail.com>
2023-04-08 16:13:58 +0200

    abdultawab add cart and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Componants/home/home.component';
+import { NotFoundComponent } from './Componants/not-found/not-found.component';
+import { UserCartComponent } from './Componants/user-cart/user-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'cart',component:UserCartComponent,title:'cart'},
+  {path:'',component:HomeComponent,children:[
+    {path:'',redirectTo:'/Home',pathMatch:'full'},//Default path
+    {path:'Home',component:HomeComponent,title:'Home'},
+    // {path:'cart',component:UserCartComponent,title:'cart'},
+  ]},
+  {path:'**',component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeaderComponent } from './Componants/header/header.component';
 import { FooterComponent } from './Componants/footer/footer.component';
 import { SideBarComponent } from './Componants/side-bar/side-bar.component';
 import { HomeComponent } from './Componants/home/home.component';
+import { UserCartComponent } from './Componants/user-cart/user-cart.component';
+import { NotFoundComponent } from './Componants/not-found/not-found.component';
+import { MainLayoutComponent } from './Componants/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { HomeComponent } from './Componants/home/home.component';
     HeaderComponent,
     FooterComponent,
     SideBarComponent,
-    HomeComponent
+    HomeComponent,
+    UserCartComponent,
+    NotFoundComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
