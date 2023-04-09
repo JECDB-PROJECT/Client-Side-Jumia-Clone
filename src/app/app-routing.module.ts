ab6449cc57373fa20d0f7b8cccc510a924bc62bc
hussein sayed <sayedhussein811@gmail.com>
2023-04-09 22:44:43 +0200

    sub rout to sing up and sign out

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './Componants/home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
+  { path: 'Home', component: HomeComponent },
+  {
+    path: 'User',
+    loadChildren: () => import('./Componants/user/user.module').then(m => m.UserModule)
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
