36cf76501e525687532f27f8775dcbef63bf463a
hussein sayed <sayedhussein811@gmail.com>
2023-04-09 22:48:05 +0200

    app module for handel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,10 @@ import { HeaderComponent } from './Componants/header/header.component';
 import { FooterComponent } from './Componants/footer/footer.component';
 import { SideBarComponent } from './Componants/side-bar/side-bar.component';
 import { HomeComponent } from './Componants/home/home.component';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,10 +18,20 @@ import { HomeComponent } from './Componants/home/home.component';
     FooterComponent,
     SideBarComponent,
     HomeComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
