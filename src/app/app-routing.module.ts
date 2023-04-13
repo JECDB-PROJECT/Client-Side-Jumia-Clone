fb3e22cc3b86fce524bea35b9996e45dae33913d
hussein sayed <sayedhussein811@gmail.com>
2023-04-13 14:27:45 +0200

    adding routs for new componants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,25 @@ import { UserCartComponent } from './Componants/user-cart/user-cart.component';
 import { HelpCenterComponent } from './Componants/help-center/help-center.component';
 import { UseraccountComponent } from './Componants/useraccount/useraccount.component';
 import { AccountoverviewComponent } from './accountoverview/accountoverview.component';
+import { CategProductsComponent } from './Componants/categ-products/categ-products.component';
+import { SubCategProductsComponent } from './Componants/sub-categ-products/sub-categ-products.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
-  { path: 'Home', component: HomeComponent }, {
-    path: 'cart', component: UserCartComponent
-  }, { path: 'help', component: HelpCenterComponent }, { path: 'useraccount', component: UseraccountComponent, children: [{ path: 'accountoverview', component: AccountoverviewComponent },] },
+  { path: 'Home', component: HomeComponent },
+  { path: 'cart', component: UserCartComponent },
+  { path: 'help', component: HelpCenterComponent },
+  {
+    path: 'useraccount', component: UseraccountComponent, children: [
+      { path: 'accountoverview', component: AccountoverviewComponent },
+    ]
+  },
   {
     path: 'User',
     loadChildren: () => import('./Componants/user/user.module').then(m => m.UserModule)
   },
+  { path: 'category/:id', component: CategProductsComponent },
+  { path: 'subcategory/:id', component: SubCategProductsComponent }
 ];
 
 @NgModule({
