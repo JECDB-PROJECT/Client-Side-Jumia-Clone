1c1babca7da62037aee441b3d78b2487ab90942f
hussein sayed <sayedhussein811@gmail.com>
2023-04-15 15:42:08 +0200

    nwe service for account to edite

---
+++ src/app/Services/user/acountuser.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Iuser } from 'src/app/Models/iuser';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AcountuserService {
+  private httpOptions = {};
+  constructor(private http: HttpClient) {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+  }
+
+  getUserByID(Id: string): Observable<Iuser> {
+
+
+    return this.http.get<Iuser>(environment.jDBUrl + '/api/users/' + Id);
+
+  }
+
+  updateUser(Id: string, data: any): Observable<Iuser> {
+    return this.http.put<Iuser>(
+      environment.jDBUrl + '/api/users/' + Id,
+      data,
+      this.httpOptions
+    );
+  }
+  checkEmail(email: string): Observable<any> {
+    return this.http.get<any>( environment.jDBUrl + '/api/users/checkEmail/' + email);
+  }
+}
