db50887040efecad03823d646bb418c47fd8cafd
hussein sayed <sayedhussein811@gmail.com>
2023-04-15 15:44:00 +0200

    new edite in header comm

--- src/app/Componants/header/header.component.ts
+++ src/app/Componants/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProductServicesService } from 'src/app/Services/productservices/product-services.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  cartProducts:any;
+  userId: string="6425633cf68f40eb571fff5f";
+  subscription!:Subscription;
+  constructor(private prdservice: ProductServicesService){
 
+  }
+  ngOnInit(): void {
+    this.subscription = this.prdservice.getUserCart().subscribe(data => {
+      console.log('user data is *********> ', data);
+      this.cartProducts = data[0];
+  })
+
+  }
 }
