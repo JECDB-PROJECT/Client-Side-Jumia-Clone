4fb5e282c49da465b402587270e1b2209d38f2b1
hussein sayed <sayedhussein811@gmail.com>
2023-04-15 15:41:02 +0200

    handling service for cart

--- src/app/Services/productservices/product-services.service.ts
+++ src/app/Services/productservices/product-services.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable ,throwError} from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
@@ -15,9 +15,9 @@ import { Iproduct } from 'src/app/Models/iproduct';
   providedIn: 'root'
 })
 export class ProductServicesService {
-   loginUserId:string;
+  loginUserId: string;
 
-   handleError(error: HttpErrorResponse) {
+  handleError(error: HttpErrorResponse) {
     let msg = '';
     if (error.error instanceof ErrorEvent) {
       // client-side error
@@ -37,51 +37,53 @@ export class ProductServicesService {
     public router: Router,
     private storageService: LocalStorgeService
   ) {
-    this.loginUserId="6425633cf68f40eb571fff5f";
+    this.loginUserId = "6425633cf68f40eb571fff5f";
     this.httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
       })
     }
-   }
+  }
 
-   getUserCart(userID: any): Observable<Icart> {
-    return this.http.get<Icart>(`${environment.jDBUrl}/api/cart/${this.loginUserId}`, this.httpOptions).pipe(
+  getUserCart(): Observable<Icart> {
+    return this.http.get<Icart>(`${environment.jDBUrl}/api/cart/userCart`, this.httpOptions).pipe(
       catchError(this.handleError)
     )
 
   }
 
-  addUserCart(cart : any): Observable<Icart> {
-    return this.http.post<Icart>(`${environment.jDBUrl}/api/cart/addToCart`,cart, this.httpOptions).pipe(
+  addUserCart(cart: any): Observable<Icart> {
+    return this.http.post<Icart>(`${environment.jDBUrl}/api/cart/addToCart`, cart, this.httpOptions).pipe(
       catchError(this.handleError)
     )
 
   }
 
-  removeSpecificCart(userID: any,productId:any): Observable<Icart> {
-    return this.http.post<Icart>(`${environment.jDBUrl}/api/cart/${productId}`,userID, this.httpOptions).pipe(
+  removeSpecificCart(userID: any, productId: any): Observable<Icart> {
+    return this.http.post<Icart>(`${environment.jDBUrl}/api/cart/${productId}`, userID, this.httpOptions).pipe(
       catchError(this.handleError)
     )
 
   }
 
-
+  deleteItemFromCart(cartId: string, productId: string): Observable<Icart> {
+    return this.http.delete<Icart>(environment.jDBUrl + '/api/cart/' + cartId + '/' + productId)
+  }
 
   GetProductsBySubCate(subcate: string): Observable<Iproduct[]> {
     return this.http.get<Iproduct[]>(
       environment.jDBUrl + '/api/products/subcat/' + subcate, this.httpOptions).pipe(
         catchError(this.handleError)
       )
-  
+
   }
-  
+
   getProductsByCategory(cate: string) {
     return this.http.get<Iproduct[]>(
       environment.jDBUrl + '/api/products/cat/' + cate, this.httpOptions).pipe(
         catchError(this.handleError)
       )
-  
+
   }
 
 
